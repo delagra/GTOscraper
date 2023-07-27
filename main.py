549700be55b0f4573a63af4f4a9d28914a436ec1
Vlade Shestakov <vlade@delagra.com>
2023-07-26 22:52:38 -0400

    implemented writing to excel

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ def locate_solver():
     pag.click(gto_coord)
 def scrape_solve():
     #find and click the solver icon
-    locate_solver()
+
 
     #find and click the combos window
     combos_coord = pag.locateCenterOnScreen('combos.PNG',grayscale=True, confidence = 0.9)
     while combos_coord is None:
+        locate_solver()
         time.sleep(1)
         combos_coord = pag.locateCenterOnScreen('combos.PNG', grayscale=True, confidence=0.9)
     print(combos_coord)
@@ -52,28 +53,37 @@ def scrape_solve():
     pag.press('c')
     pag.keyUp('ctrl')
     bigdf = pd.read_clipboard(sep='\t')
+    pag.press('esc')
     print(bigdf)
     return df, bigdf
 
-def generate_excel():
-    wb = op.load_workbook('Example 3-  Test all types of data .xlsx')
-    ws = wb.active
-    print(ws['B3'].value)
+wb = op.load_workbook('expanded template.xlsx')
+ws = wb.active
+
     #print(sheet_ranges)
+
+def get_statistic(_df,stat):
+    try:
+        calc = _df[_df['Statistic']==stat].values[0][1:5]
+    except IndexError:
+        print(stat,"not found")
+        return
+    output = [round(calc[0])]
+    for val in calc[1:]:
+        if not pd.isna(val):
+            output.append(round(val/calc[0]*100))
+        else:
+            output.append(val)
+    return output
 
 class Sim: #class that will contain all sim results to be populated in excel later
     def __init__(self, board):
         self.board = board
-    def is_monotone(self):
-        if self.board[1] == self.board[3] == self.board[5]:
-            return True
-        else:
-            return False
-    def is_twotone(self):
-        if self.board[1] == self.board[3] or self.board[1] == self.board[5] or self.board[3] == self.board[5]:
-            return True
-        else:
-            return False
+    is_monotone = False
+    is_two_tone1 = False
+    is_two_tone2 = False
+    is_two_tone3 = False
+    is_rainbow = False
     ace_high = []
     overpair = []
     top_pair = []
@@ -88,9 +98,47 @@ class Sim: #class that will contain all sim results to be populated in excel lat
     combos = []
 
 def populate_class(_sim,df1, df2):
-    setattr(_sim, 'combos', [df1['c'][0],df1['d'][0],df1['e'][0]])
+    options = {
+        'top_pair': 'top pair',
+        'overpair': 'overpair',
+        'mid_pair': 'middle pair',
+        'ace_high': 'ace high',
+        'gutshot': 'gutshot',
+        'overcards': 'overcards',
+        'nut_fd': 'nut flushdr.',
+        'weak_fd': 'weak flushdr.',
+        'set': 'sets',
+        'oesd': 'oesd'
+    }
+    for key, value in options.items():
+        setattr(_sim, key, get_statistic(df2, value))
+    setattr(_sim,'first_action', [df1['c'][1],df1['b'][1]])
+    setattr(_sim,'combos', [df1['b'][0]])
+    '''
+    setattr(_sim, 'top_pair', get_statistic(df2,'top pair'))
+    setattr(_sim, 'overpair', get_statistic(df2, 'overpair'))
+    setattr(_sim, 'mid_pair', get_statistic(df2, 'middle pair'))
+    setattr(_sim, 'ace_high', get_statistic(df2, 'ace high'))
+    setattr(_sim, 'gutshot', get_statistic(df2, 'gutshot'))
+    setattr(_sim, 'overcards', get_statistic(df2, 'overcards'))
+    setattr(_sim, 'nut_fd', get_statistic(df2, 'nut flushdr.'))
+    setattr(_sim, 'weak_fd', get_statistic(df2, 'weak flushdr.'))
+    setattr(_sim, 'set', get_statistic(df2, 'sets'))
+    setattr(_sim, 'oesd', get_statistic(df2, 'oesd'))
+    '''
 
-def openfile(file:str):
+def openfile(file:str, sim_instance):
+    if "m" in file:
+        setattr(sim_instance, 'is_monotone', True)
+    if "-1" in file:
+        setattr(sim_instance, 'is_two_tone1', True)
+    if "-2" in file:
+        setattr(sim_instance, 'is_two_tone2', True)
+    if "-3" in file:
+        setattr(sim_instance, 'is_two_tone3', True)
+    if "r" in file:
+        setattr(sim_instance, "is_rainbow", True)
+
     locate_solver()
     pag.keyDown('ctrl')
     pag.press('o')
@@ -100,18 +148,52 @@ def openfile(file:str):
     pag.press('enter')
 
 
+def find_excel_location(value,obj,excel):
+    colQC = excel['QC']
+    for row in colQC:
+        if row.value == value:
+            base_loc = row
+    if obj.is_monotone:
+        return base_loc.offset(column=-442)
+    elif obj.is_rainbow:
+        return base_loc.offset(column=444)
+    elif obj.is_two_tone2:
+        return base_loc.offset(column=1, row=1)
+    elif obj.is_two_tone3:
+        return base_loc.offset(column=1, row=2)
+    else:
+        return base_loc.offset(column=1)
+
+
+def sim_to_excel(base_location,_sim, excel):
+    for index,val in enumerate(_sim.first_action):
+        base_location.offset(column=index).value = val
 
 
 
-sheet_locations = {"monotone": "c2", "twotone1":"f2","twotone2": "x2","twotone3":"x6","rainbow":"aio1"}
+
+
 
 #generate_excel()
-'''
-df1, df2 = scrape_solve()
+#openfile("998r",sim)
+
+
 sim = Sim
-populate_class(sim,df1,df2)
-print(sim.combos)
+openfile("998r", sim)
+df1, df2 = scrape_solve()
+populate_class(sim, df1, df2)
+print(sim.nut_fd)
+print(sim.combos, sim.first_action)
+
+loc = find_excel_location('AAK', sim, ws)
+print(loc)
+sim_to_excel(loc,sim,ws)
+wb.save('test.xslx')
 '''
 df1, df2 = scrape_solve()
 df2.to_csv('gto.csv')
+
 #openfile("akqm")
+'''
+
+
